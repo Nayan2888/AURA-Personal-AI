efdd77b64b9333574d195247f610ce7179d78cda
Nayan2888 <nayanrajgor2888@gmail.com>
2026-09-19 20:12:30 +0530

    fix: migrate Android Kotlin compiler options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,12 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = "17" }
+ }
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
